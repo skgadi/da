129e7281acec6707b011882805b5b4cfd6413dcb
Suresh Kumar Gadi <skgadi@users.noreply.github.com>
2022-12-03 00:33:48 -0600

    Automatic push

--- dev/ver004/gen-exe.bat
+++ dev/ver004/gen-exe.bat
@@ -1,1 +1,1 @@
-pyinstaller --distpath "../../bin/" --onefile --add-data "./logo.ico;." --add-data "./logo.png;." --icon="logo.png" main.pyw
+pyinstaller --distpath "../../bin/" --onefile --add-data "./logo.ico;." --add-data "./logo.png;." --icon="logo.png" -n "da" main.pyw
